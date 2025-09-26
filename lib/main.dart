a494919abeef530e7a255d17d74ce98093f6477f
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-09-26 21:12:32 +0530

    test: add animations in buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:pantry_ai/core/theme/theme.dart';
-import 'package:pantry_ai/features/auth/domain/repository/auth_repository_impl.dart';
 
-import 'config/routes.dart';
-import 'features/auth/presentation/bloc/auth_bloc.dart';
+import 'animations_implicit/animated_color_pallete.dart';
+import 'animations_implicit/animated_shopping_cart_button.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -18,46 +12,61 @@ void main() async {
   runApp(const MyApp());
 }
 
-
-
-
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
-  final FirebaseAuth firebase_auth = FirebaseAuth.instance;
-  final GoogleSignIn google_sign_in = GoogleSignIn();
-
-  @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => AuthBloc(
-            authRepository: AuthRepositoryImpl(
-              firestore: firestore,
-              firebaseAuth: firebase_auth,
-              googleSignIn: google_sign_in,
-            ),
-          ),
-        ),
-      ],
-      child: Builder(
-        builder: (context) {
-          return MaterialApp.router(
-            routerConfig: createRouter(),
-            theme: lightTheme,
-            darkTheme: darkTheme,
-            themeMode: ThemeMode.system,
-            debugShowCheckedModeBanner: false,
-          );
-        },
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const ShoppingCartButton(),
     );
   }
 }
+
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   final FirebaseFirestore firestore = FirebaseFirestore.instance;
+//   final FirebaseAuth firebase_auth = FirebaseAuth.instance;
+//   final GoogleSignIn google_sign_in = GoogleSignIn();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MultiBlocProvider(
+//       providers: [
+//         BlocProvider(
+//           create: (context) => AuthBloc(
+//             authRepository: AuthRepositoryImpl(
+//               firestore: firestore,
+//               firebaseAuth: firebase_auth,
+//               googleSignIn: google_sign_in,
+//             ),
+//           ),
+//         ),
+//       ],
+//       child: Builder(
+//         builder: (context) {
+//           return MaterialApp.router(
+//             routerConfig: createRouter(),
+//             theme: lightTheme,
+//             darkTheme: darkTheme,
+//             themeMode: ThemeMode.system,
+//             debugShowCheckedModeBanner: false,
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
