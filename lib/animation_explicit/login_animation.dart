0c7d27d3094c5fc6b9a1a6f16c42277ec011f81d
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-10-01 19:02:05 +0530

    feat: add explicit animations

--- lib/animation_explicit/login_animation.dart
+++ lib/animation_explicit/login_animation.dart
@@ -11,12 +11,27 @@ class _LoginScreenAnimationState extends State<LoginScreenAnimation>
     with SingleTickerProviderStateMixin {
   late Animation<double> logoFadeAnimation;
   late AnimationController controller;
+  late Animation<Offset> slideAnimation;
+  late Animation<double> scaleAnimation;
 
   @override
   void initState() {
     super.initState();
-    controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 1500));
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
     logoFadeAnimation = Tween<double>(begin: 0, end: 1).animate(controller);
+    slideAnimation = Tween(
+      begin: const Offset(-1, -1),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: controller, curve: Curves.ease));
+
+    scaleAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(CurvedAnimation(parent: controller, curve: Curves.ease));
+
     controller.forward();
   }
 
@@ -29,23 +44,32 @@ class _LoginScreenAnimationState extends State<LoginScreenAnimation>
           children: [
             FadeTransition(
               opacity: logoFadeAnimation,
-                child: const FlutterLogo(size: 100.0)
+              child: const FlutterLogo(size: 100.0),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                children: [
-                  const TextField(
-                    decoration: InputDecoration(labelText: 'Username'),
+              child: SlideTransition(
+                position: slideAnimation,
+                child: ScaleTransition(
+                  scale: scaleAnimation,
+                  child: Column(
+                    children: [
+                      const TextField(
+                        decoration: InputDecoration(labelText: 'Username'),
+                      ),
+                      const SizedBox(height: 16.0),
+                      const TextField(
+                        decoration: InputDecoration(labelText: 'Password'),
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 16.0),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Login'),
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 16.0),
-                  const TextField(
-                    decoration: InputDecoration(labelText: 'Password'),
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 16.0),
-                  ElevatedButton(onPressed: () {}, child: const Text('Login')),
-                ],
+                ),
               ),
             ),
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'animation_explicit/login_animation.dart';
+import 'animation_explicit/list_animation.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginScreenAnimation(),
+      home: const ListAnimation(),
     );
   }
 }
